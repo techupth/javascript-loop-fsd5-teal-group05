4edbad52c6dcbc331ea96ba1843f068857f8ce1e
Dennis <suwatchai.kaew@gmail.com>
2023-11-30 16:13:19 +0700

    feat: add finished ex1-6 V1

--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,10 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
-
+let i = 0;
+let length = restaurants.length;
+while (i < length) {
+  newRestaurants.push("Restaurant Name: " + restaurants[i]);
+  i++;
+}
 console.log(newRestaurants);
