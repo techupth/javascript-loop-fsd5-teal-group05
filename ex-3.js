8929a2fc666fdd2b3fd73cadd87af941682bc666
Krit <titatit.akkh@gmail.com>
2023-12-01 13:43:49 +0700

    Feat: Finish ex black hole loop

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,17 @@
 let numbers = [100, 20, 3, 1000];
 let minNumber;
 // Start coding here
+minNumber = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    if(numbers[i] < numbers.length) {
+        minNumber.push(numbers[i]);;
+    }
+}
+
+// for (let i of numbers) {
+//     const newNumber = i < numbers.length;
+//     minNumber.push(newNumber);
+// }
 
 console.log(minNumber);
--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,11 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
+let i = 0;
+let text = "Restaurants Name: ";
+while (restaurants[i]) {
+    newRestaurants.push(text += restaurants[i]);
+    i++;
+}
 
 console.log(newRestaurants);
