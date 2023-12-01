5d605fd8c004bce6019d4b86dd5402405dbf96d5
suphattraying <suphattra.spk@gmail.com>
2023-12-01 12:20:58 +0700

    finish

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,11 @@
 // Exercise #3: Find a Minimum Number
 let numbers = [100, 20, 3, 1000];
-let minNumber;
-// Start coding here
+let minNumber ;
 
-console.log(minNumber);
+// Start coding here
+for(let i=0;i< numbers.length;i++){
+   if(numbers[i] = Math.min(...numbers)){
+     minNumber = numbers[i]
+}}
+console.log(minNumber)
+
